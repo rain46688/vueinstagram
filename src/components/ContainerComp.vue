273da0d7c70048cea56418e4062bf69323c83cc2
rain46688 <rain46688@gmail.com>
2022-07-16 21:06:49 +0900

    step1

---
+++ src/components/ContainerComp.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <PostComp v-for="(data, num) in instagramData" :key="num" :data='data'/>
+    </div>
+</template>
+
+<script>
+
+import PostComp from './PostComp.vue';
+
+export default {
+    name : 'ContainerComp',
+    props : {
+        instagramData : Array,
+    },
+    components : {
+        PostComp
+    }
+
+}
+</script>
+
+<style>
+</style>
